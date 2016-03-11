9266f80554abb246c48c2a4154ef6903b355b18b
liurl3 <liurl3@asiainfo.com>
2016-03-11 17:56:27 +0800

    implement interface

--- engagedparty/components/com.digiwes.frameworx.engagedparty.party.api/src/main/java/com/digiwes/frameworx/engagedparty/party/api/interfaces/IndividualFactory.java
+++ engagedparty/components/com.digiwes.frameworx.engagedparty.party.api/src/main/java/com/digiwes/frameworx/engagedparty/party/api/interfaces/IndividualFactory.java
@@ -4,10 +4,11 @@ import com.digiwes.frameworx.engagedparty.party.bean.Individual;
 import org.apache.olingo.commons.api.data.Entity;
 
 import java.beans.IntrospectionException;
+import java.lang.reflect.InvocationTargetException;
 
 public interface IndividualFactory{
 	
-	public Individual convertEntity(Entity entity) throws IntrospectionException;
+	public Individual convertEntity(Entity entity) throws IntrospectionException, InvocationTargetException, IllegalAccessException;
 	
 	public Entity convertBean(Individual individual) throws IntrospectionException;
 	
--- engagedparty/components/com.digiwes.frameworx.engagedparty.party.api/src/main/java/com/digiwes/frameworx/engagedparty/party/api/service/IndividualResourceImpl.java
+++ engagedparty/components/com.digiwes.frameworx.engagedparty.party.api/src/main/java/com/digiwes/frameworx/engagedparty/party/api/service/IndividualResourceImpl.java
@@ -1,24 +1,19 @@
 package com.digiwes.frameworx.engagedparty.party.api.service;
 
 import com.digiwes.frameworx.common.basetype.TimePeriod;
-import com.digiwes.frameworx.engagedparty.party.bean.Individual;
+import com.digiwes.frameworx.engagedparty.party.bean.*;
 import com.digiwes.frameworx.engagedparty.party.api.interfaces.IndividualFactory;
-import com.digiwes.frameworx.engagedparty.party.bean.IndividualName;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Service;
-import org.apache.olingo.commons.api.data.ComplexValue;
-import org.apache.olingo.commons.api.data.Entity;
-import org.apache.olingo.commons.api.data.Property;
-import org.apache.olingo.commons.api.data.ValueType;
+import org.apache.olingo.commons.api.data.*;
 
 import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 
 /**
@@ -28,31 +23,68 @@ import java.util.Set;
 @Service(value = IndividualFactory.class)
 public class IndividualResourceImpl implements IndividualFactory {
     @Override
-    public Individual convertEntity(Entity entity) throws IntrospectionException {
+    public Individual convertEntity(Entity entity) throws IntrospectionException, InvocationTargetException, IllegalAccessException {
         Individual individual = new Individual();
-        PropertyDescriptor[] propertyDescriptors =  getPropertyDescriptor(individual);
         List<Property> properties = entity.getProperties();
-        for(int i = 0 ;i < properties.size() ; i++){
-            Property property = properties.get(i);
-            String propertyName = property.getName();
-            Object propertyValue = property.getValue();
-            for(int j = 0 ; j < propertyDescriptors.length ; j++){
-                String beanPropertyName = propertyDescriptors[j].getName();
-                if(propertyName.equals(beanPropertyName)){
-                   // propertyDescriptors[j].getWriteMethod().invoke(individual,propertyValue);
-                    propertyDescriptors[j].setValue(beanPropertyName, propertyValue);
-                    break;
+        for( Property property : properties){
+            if(null != property){
+                String propertyName = property.getName();
+                Object propertyValue = property.getValue();
+                PropertyDescriptor descriptor = new PropertyDescriptor(propertyName,individual.getClass());
+                        if("validFor".equals(propertyName)){
+                            ComplexValue complexValue = (ComplexValue)propertyValue;
+                            TimePeriod obj = createBeanProperty(complexValue, new TimePeriod());
+                            descriptor.getWriteMethod().invoke(individual,obj);
+                        }else if("_languageAbilitys".equals(propertyName)){
+
+                            ComplexValue complexValue = (ComplexValue)propertyValue;
+                            createBeanProperty(complexValue,new LanguageAbility());
+                        }else if("_defaultIndividualName".equals(propertyName)){
+                            ComplexValue complexValue = (ComplexValue)propertyValue;
+                            DefaultIndividualName obj = createBeanProperty(complexValue, new DefaultIndividualName());
+                            descriptor.getWriteMethod().invoke(individual, obj);
+                        }else if("_optionalIndividualName".equals(propertyName)){
+                            ComplexValue complexValue = (ComplexValue)propertyValue;
+                            OptionalIndividualName optName = createBeanProperty(complexValue, new OptionalIndividualName());
+                        }else if("aliveDuring".equals(propertyName)){
+                            ComplexValue complexValue = (ComplexValue)propertyValue;
+                            TimePeriod obj = createBeanProperty(complexValue, new TimePeriod());
+                            descriptor.getWriteMethod().invoke(individual,obj);
+                        }else{
+                            propertyValue.getClass();
+                            if (propertyValue instanceof String) {
+                                descriptor.getWriteMethod().invoke(individual, (String) propertyValue);
+                            } else if(propertyValue instanceof Date){
+                                descriptor.getWriteMethod().invoke(individual, (Date) propertyValue);
+                            }
+                        }
+            }
+        }
+        System.out.println(individual);
+        return individual;
+    }
+
+    private <T> T createBeanProperty(ComplexValue complexValue,T bean) throws IntrospectionException, InvocationTargetException, IllegalAccessException {
+        if(null != complexValue){
+            List<Property> properties = complexValue.getValue();
+            for( Property property : properties) {
+                if (null != property) {
+                    String propertyName = property.getName();
+                    Object propertyValue = property.getValue();
+                    if(!"class".equals(propertyName)){
+                        if(null != bean){
+                            PropertyDescriptor descriptor = new PropertyDescriptor(propertyName,bean.getClass());
+                            if (propertyValue instanceof String) {
+                                descriptor.getWriteMethod().invoke(bean, (String) propertyValue);
+                            } else if(propertyValue instanceof Date){
+                                descriptor.getWriteMethod().invoke(bean, (Date) propertyValue);
+                            }
+                        }
+                    }
                 }
             }
         }
-        return individual;
-    }
-    private PropertyDescriptor[] getPropertyDescriptor(Individual bean) throws IntrospectionException {
-        Class type = bean.getClass();
-        Entity entity = new Entity();
-        BeanInfo beanInfo = Introspector.getBeanInfo(type);
-        PropertyDescriptor[] propertyDescriptors =  beanInfo.getPropertyDescriptors();
-        return propertyDescriptors;
+        return bean;
     }
     @Override
     public Entity convertBean(Individual bean) throws IntrospectionException {
@@ -63,39 +95,33 @@ public class IndividualResourceImpl implements IndividualFactory {
         for (int i = 0; i< propertyDescriptors.length; i++) {
             PropertyDescriptor descriptor = propertyDescriptors[i];
             String propertyName = descriptor.getName();
-            Object value = getPropertyValue(bean, propertyName);
             Property property = null;
-            /*if (value instanceof TimePeriod) {
-                property = createComplexProperty(propertyName, value);
-            } else if (value instanceof String) {
-                property = createPrimitive(propertyName,value);
-            } else if (value instanceof IndividualName) {
-                property = createComplexProperty(propertyName, value);
-            } else if (value instanceof List) {
-                //property =
-            }*/
-            if (value instanceof String) {
-                property = createPrimitive(propertyName,value);
-            } else if (value instanceof Integer) {
-                property = createPrimitive(propertyName, value);
-            }else if (value instanceof Boolean){
-                property = createPrimitive(propertyName,value);
-            }else if(value instanceof Float){
-                property = createPrimitive(propertyName,value);
-            }else if(value instanceof Double){
-                property = createPrimitive(propertyName,value);
-            }else if(value instanceof Long){
-                property = createPrimitive(propertyName,value);
-            }else if(value instanceof Set){
-                Set setValue =(Set) value;
-                Iterator it = setValue.iterator();
-                while(it.hasNext()){
-                    Object obj = it.next();
-                    property = createComplexProperty(propertyName, obj);
-                }
-            }else{
-                if(!"class".equals(propertyName)){
-                    property = createComplexProperty(propertyName, value);
+            if(!"class".equals(propertyName)){
+                Object value = getPropertyValue(bean, propertyName);
+                if (value instanceof String) {
+                    property = createPrimitive(propertyName,value);
+                } else if (value instanceof Integer) {
+                    property = createPrimitive(propertyName, value);
+                }else if (value instanceof Boolean){
+                    property = createPrimitive(propertyName,value);
+                }else if(value instanceof Float){
+                    property = createPrimitive(propertyName,value);
+                }else if(value instanceof Double){
+                    property = createPrimitive(propertyName,value);
+                }else if(value instanceof Long){
+                    property = createPrimitive(propertyName,value);
+                }else if(value instanceof Set){
+                    Set setValue =(Set) value;
+                    Iterator it = setValue.iterator();
+                    if(setValue.size()<1){
+                        property = createComplexProperty(propertyName, null);
+                    }
+                    while(it.hasNext()){
+                        Object obj = it.next();
+                        property = createComplexProperty(propertyName, obj);
+                    }
+                }else{
+                        property = createComplexProperty(propertyName, value);
                 }
             }
             entity.addProperty(property);
@@ -104,7 +130,6 @@ public class IndividualResourceImpl implements IndividualFactory {
     }
     private Property createComplexProperty(String name, Object bean) throws IntrospectionException {
         ComplexValue complexValue=new ComplexValue();
-        List<Property> addressProperties = complexValue.getValue();
         if(null != bean){
             Class type = bean.getClass();
             Entity entity = new Entity();
@@ -113,8 +138,10 @@ public class IndividualResourceImpl implements IndividualFactory {
             for (int i = 0; i< propertyDescriptors.length; i++) {
                 PropertyDescriptor descriptor = propertyDescriptors[i];
                 String propertyName = descriptor.getName();
-                Object value = getPropertyValue(bean, propertyName);
-                complexValue.getValue().add(createPrimitive(propertyName, value));
+                if(!"class".equals(propertyName)){
+                    Object value = getPropertyValue(bean, propertyName);
+                    complexValue.getValue().add(createPrimitive(propertyName, value));
+                }
             }
         }
         return  new Property(null,name, ValueType.COMPLEX, complexValue);
@@ -135,4 +162,5 @@ public class IndividualResourceImpl implements IndividualFactory {
             return null;
         }
     }
+
 }
